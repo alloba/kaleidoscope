615b5cad3a530bdd606ccd3a60c011848d526aaf
alloba <alexlbates@gmail.com>
2021-02-03 17:30:15 -0500

    update cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,12 +12,12 @@ let fileInformationPath = '/projects/kaleidoscope/testinfo.json' //both of these
 
 processStartupArgs();
 const app = express();
-app.use(cors());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-})
-app.options('*', cors());
+// app.use(cors());
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// })
+// app.options('*', cors());
 app.use(express.json());
 
 const imageService = new ImageService(imageDirectory);
