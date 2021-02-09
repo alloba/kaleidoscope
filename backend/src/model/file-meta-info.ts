f65a76e5fcf634508a549fac30218f6779e0481e
Alex Bates <abates@kinsaleins.com>
2021-02-09 12:01:45 -0500

    remove old node backend in favor of the spring-boot one

--- backend/src/model/file-meta-info.ts
+++
@@ -1,9 +0,0 @@
-/**
- * hold meta information for a particular file. 
- * This is meant to typically be wrapped inside of the FileInformation class, associated to a filename.
- */
-export default class FileMetaInfo{
-    tags: Array<string> = [];
-    description: string = '';
-    songName: string = '';
-}
