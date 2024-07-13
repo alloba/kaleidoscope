744437f0f0b482260c32f90fca1818bad2c2cafb
Alex Bates <alexlbates@gmail.com>
2024-07-13 13:06:00 -0400

    remove angular completely. replicate all functionality in vanilla js.

--- src/app/model/file-meta.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FileMeta } from './file-meta';
-
-describe('FileMeta', () => {
-  it('should create an instance', () => {
-    expect(new FileMeta()).toBeTruthy();
-  });
-});
--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  bucketUrlPath: 'https://kaleidoscope-media.s3.us-east-1.amazonaws.com/',
-  awsRegion: 'us-east-1',
-  awsBucket: 'kaleidoscope-media',
-  //these aws credentials are for RO S3 access. Meaning, it matters 0% if someone skims this as far as i am concerned.
-  awsAccessKey: 'AKIA6AVV5VSCJOMOZI5J',
-  awsSecretKey: '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'
-};
